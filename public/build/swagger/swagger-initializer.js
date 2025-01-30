2c23a4d1417f2bd22ba008317af181619d84da13
Regy Saputra <regysaputra27@gmail.com>
2025-01-30 19:35:06 +0700

    refactor build directory

--- public/build/swagger/swagger-initializer.js
+++ public/build/swagger/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://contact-api-sigma.vercel.app/swagger_ui/yaml/docs.yaml",
+    url: "https://contact-api-sigma.vercel.app/build/swagger/docs.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
--- resources/views/swagger.blade.php
+++ resources/views/swagger.blade.php
@@ -6,15 +6,15 @@
   <head>
     <meta charset="UTF-8">
     <title>Swagger UI</title>
-    <link rel="stylesheet" type="text/css" href="{{ asset('build/assets/swagger-ui.css') }}" />
-    <link rel="icon" type="image/png" href="{{ asset('build/assets/favicon-32x32.png') }}" sizes="32x32" />
-    <link rel="icon" type="image/png" href="{{ asset('build/assets/favicon-16x16.png') }}" sizes="16x16" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('build/swagger/swagger-ui.css') }}" />
+    <link rel="icon" type="image/png" href="{{ asset('build/swagger/favicon-32x32.png') }}" sizes="32x32" />
+    <link rel="icon" type="image/png" href="{{ asset('build/swagger/favicon-16x16.png') }}" sizes="16x16" />
   </head>
 
   <body>
     <div id="swagger-ui"></div>
-    <script src="{{ asset('build/assets/swagger-ui-bundle.js') }}" charset="UTF-8"> </script>
-    <script src="{{ asset('build/assets/swagger-ui-standalone-preset.js') }}" charset="UTF-8"> </script>
-    <script src="{{ asset('build/assets/swagger-initializer.js') }}" charset="UTF-8"> </script>
+    <script src="{{ asset('build/swagger/swagger-ui-bundle.js') }}" charset="UTF-8"> </script>
+    <script src="{{ asset('build/swagger/swagger-ui-standalone-preset.js') }}" charset="UTF-8"> </script>
+    <script src="{{ asset('build/swagger/swagger-initializer.js') }}" charset="UTF-8"> </script>
   </body>
 </html>
