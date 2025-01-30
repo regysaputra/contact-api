29a9b72146b2398dbd8a807c9b5cf56dd43cd6b1
Regy Saputra <regysaputra27@gmail.com>
2025-01-30 10:41:14 +0700

    edit package.json

--- resources/views/swagger.blade.php
+++ resources/views/swagger.blade.php
@@ -1,19 +1,20 @@
+@use('Illuminate\Support\Facades\Vite')
+
 <!-- HTML for static distribution bundle build -->
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
   <head>
     <meta charset="UTF-8">
     <title>Swagger UI</title>
-    <link rel="stylesheet" type="text/css" href="{{ asset('swagger_ui/css/swagger-ui.css') }}" />
-    <link rel="stylesheet" type="text/css" href="index.css" />
-    <link rel="icon" type="image/png" href="{{ asset('swagger_ui/images/favicon-32x32.png') }}" sizes="32x32" />
-    <link rel="icon" type="image/png" href="{{ asset('swagger_ui/images/favicon-16x16.png') }}" sizes="16x16" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('build/assets/swagger-ui.css') }}" />
+    <link rel="icon" type="image/png" href="{{ asset('build/assets/favicon-32x32.png') }}" sizes="32x32" />
+    <link rel="icon" type="image/png" href="{{ asset('build/assets/favicon-16x16.png') }}" sizes="16x16" />
   </head>
 
   <body>
     <div id="swagger-ui"></div>
-    <script src="{{ asset('/swagger_ui/js/swagger-ui-bundle.js') }}" charset="UTF-8"> </script>
-    <script src="{{ asset('/swagger_ui/js/swagger-ui-standalone-preset.js') }}" charset="UTF-8"> </script>
-    <script src="{{ asset('/swagger_ui/js/swagger-initializer.js') }}" charset="UTF-8"> </script>
+    <script src="{{ asset('build/assets/swagger-ui-bundle.js') }}" charset="UTF-8"> </script>
+    <script src="{{ asset('build/assets/swagger-ui-standalone-preset.js') }}" charset="UTF-8"> </script>
+    <script src="{{ asset('build/assets/swagger-initializer.js') }}" charset="UTF-8"> </script>
   </body>
 </html>
